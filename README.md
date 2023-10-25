f0f0e1b90f07ba1d31aa1a4875a27e4f10cd2224
tobyloki <tobyloki5@gmail.com>
2023-10-24 22:53:42 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Code Explorer
+
+This app allows you to ask questions and get answers regarding your code provided the folder location of your code. The app was based on the GenAI stack from https://github.com/docker/genai-stack.
+
+NOTE: Currently app only works for code folders that are written in Python.
+
+# Configure
+
+Create a `.env` file from the environment template file `.env.example`
+
+Available variables:
+| Variable Name | Default value | Description |
+|------------------------|------------------------------------|-------------------------------------------------------------------------|
+| OLLAMA_BASE_URL | http://host.docker.internal:11434 | REQUIRED - URL to Ollama LLM API |  
+| NEO4J_URI | neo4j://database:7687 | REQUIRED - URL to Neo4j database |
+| NEO4J_USERNAME | neo4j | REQUIRED - Username for Neo4j database |
+| NEO4J_PASSWORD | password | REQUIRED - Password for Neo4j database |
+| LLM | llama2 | REQUIRED - Can be any Ollama model tag, or gpt-4 or gpt-3.5 or claudev2 |
+| EMBEDDING_MODEL | ollama | REQUIRED - Can be sentence_transformer, openai, aws or ollama |
+| AWS_ACCESS_KEY_ID | | REQUIRED - Only if LLM=claudev2 or embedding_model=aws |
+| AWS_SECRET_ACCESS_KEY | | REQUIRED - Only if LLM=claudev2 or embedding_model=aws |
+| AWS_DEFAULT_REGION | | REQUIRED - Only if LLM=claudev2 or embedding_model=aws |
+| OPENAI_API_KEY | | REQUIRED - Only if LLM=gpt-4 or LLM=gpt-3.5 or embedding_model=openai |
+| LANGCHAIN_ENDPOINT | "https://api.smith.langchain.com" | OPTIONAL - URL to Langchain Smith API |
+| LANGCHAIN_TRACING_V2 | false | OPTIONAL - Enable Langchain tracing v2 |
+| LANGCHAIN_PROJECT | | OPTIONAL - Langchain project name |
+| LANGCHAIN_API_KEY | | OPTIONAL - Langchain API key |
+
+NOTE: If using `EMBEDDING_MODEL=sentence_transformer`, uncomment code in `requirements.txt` & `chains.py`. It was commented out to reduce code size.
+
+# Docker (Linux only)
+
+**Build only**
+
+```
+docker compose --profile linux build
+```
+
+**To start everything (Linux)**
+
+```
+docker compose --profile linux up --build
+```
+
+To enter **watch mode** (auto rebuild on file changes).
+First start everything, then in new terminal:
+
+```
+docker compose --profile linux alpha watch
+```
+
+**Shutdown**
+If health check fails or containers don't start up as expected, shutdown
+completely to start up again.
+
+```
+docker compose --profile linux down
+```
+
+# Application
+
+Access the app at http://localhost:8501. In the sidebar, enter the path to your code folder and click "Process files". Then start asking questions about your code in the main chat.
+
+The database can be explored at http://localhost:7474.
---
+++ bot.py
@@ -0,0 +1,265 @@
+import os
+import streamlit as st
+from streamlit.logger import get_logger
+# import tkinter as tk
+# from tkinter import filedialog
+from langchain.callbacks.base import BaseCallbackHandler
+from dotenv import load_dotenv
+from chains import (
+    load_embedding_model,
+    load_llm,
+    configure_llm_only_chain,
+)
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.neo4j_vector import Neo4jVector
+from langchain.text_splitter import Language
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import LanguageParser
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts.prompt import PromptTemplate
+from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
+from langchain.chains import LLMChain
+from langchain.chains.question_answering import load_qa_chain
+
+# set page title
+st.set_page_config(
+    page_title="Code Explorer",
+    page_icon="👨‍💻",
+    layout="centered",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "About": "GitHub: https://github.com/tobyloki/CodeExplorer"
+    }
+)
+
+load_dotenv(".env")
+
+url = os.getenv("NEO4J_URI")
+username = os.getenv("NEO4J_USERNAME")
+password = os.getenv("NEO4J_PASSWORD")
+ollama_base_url = os.getenv("OLLAMA_BASE_URL")
+embedding_model_name = os.getenv("EMBEDDING_MODEL")
+llm_name = os.getenv("LLM")
+# Remapping for Langchain Neo4j integration
+os.environ["NEO4J_URL"] = url
+
+logger = get_logger(__name__)
+
+@st.cache_resource
+def initDB():
+    embeddings, dimension = load_embedding_model(
+        embedding_model_name, config={"ollama_base_url": ollama_base_url}, logger=logger
+    )
+
+    return embeddings
+
+@st.cache_resource
+def initLLM():
+    # create llm
+    llm = load_llm(llm_name, logger=logger, config={"ollama_base_url": ollama_base_url})
+
+    return llm
+
+embeddings = initDB()
+llm = initLLM()
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        if token.endswith('?'):
+            token += '\n\n\n'
+        token = token.replace('"', '')
+        self.text += token
+        self.container.markdown(self.text)
+
+@st.cache_resource
+def processDocuments(directory, count):
+    print("File chunking begins...")
+
+    loader = GenericLoader.from_filesystem(
+        path=directory,
+        glob="**/*",
+        suffixes=[".py"],
+        parser=LanguageParser(language=Language.PYTHON, parser_threshold=500)
+    )
+    documents = loader.load()
+    print("Total documents:", len(documents))
+
+    text_splitter = RecursiveCharacterTextSplitter.from_language(language=Language.PYTHON, 
+                                                               chunk_size=2000, 
+                                                               chunk_overlap=200)
+
+    # chunks = text_splitter.split_text(text=text)
+    chunks = text_splitter.split_documents(documents)
+    print("Chunks:", len(chunks))
+
+    hashStr = str(abs(hash(directory)))
+
+    # Store the chunks part in db (vector)
+    vectorstore = Neo4jVector.from_documents(
+        chunks,
+        url=url,
+        username=username,
+        password=password,
+        embedding=embeddings,
+        index_name=f"index_{hashStr}",
+        node_label=f"node_{hashStr}",
+        pre_delete_collection=True,  # Delete existing data
+    )
+    # qa = RetrievalQA.from_chain_type(
+    #     llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
+    # )
+
+    print("Files are now chunked up")
+
+    return vectorstore
+
+@st.cache_resource
+def getQA(_vectorstore, count):
+    custom_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. If you do not know the answer reply with 'I am sorry, but I do not know the answer to your question.'.
+    Chat History:
+    {chat_history}
+    Follow Up Input: {question}
+    Standalone question:"""
+    CUSTOM_QUESTION_PROMPT = PromptTemplate.from_template(custom_template)
+
+    question_generator = LLMChain(llm=llm, prompt=CUSTOM_QUESTION_PROMPT)
+    doc_chain = load_qa_chain(llm, chain_type="map_reduce")
+    # NOTE: sources chain doesn't answer well to question
+    # doc_chain = load_qa_with_sources_chain(llm, chain_type="map_reduce")
+
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+    qa = ConversationalRetrievalChain(
+        retriever=_vectorstore.as_retriever(search_kwargs={"k": 2}),
+        max_tokens_limit=3375,
+        question_generator=question_generator,
+        combine_docs_chain=doc_chain,
+        memory=memory,
+    )
+
+    # qa = ConversationalRetrievalChain.from_llm(
+    #     llm=llm,
+    #     retriever=_vectorstore.as_retriever(search_kwargs={"k": 2}),
+    #     max_tokens_limit=3000,
+    #     condense_question_prompt=CUSTOM_QUESTION_PROMPT,
+    #     condense_question_llm=llm,
+    #     memory=memory,
+    # )
+
+    return qa
+
+@st.cache_resource
+def getLLMChain():
+    chain = configure_llm_only_chain(llm)
+
+    return chain
+
+def main():
+    qa = None
+    llm_chain = getLLMChain()
+
+    if "directory" not in st.session_state:
+        st.session_state[f"directory"] = None
+    if "vectorstoreCount" not in st.session_state:  # only incremented to reset cache for processDocuments()
+        st.session_state[f"vectorstoreCount"] = 0
+    if "qaCount" not in st.session_state:           # only incremented to reset cache for getQA()
+        st.session_state[f"qaCount"] = 0    
+    if "user_input" not in st.session_state:
+        st.session_state[f"user_input"] = []
+    if "generated" not in st.session_state:
+        st.session_state[f"generated"] = []
+
+    # # Set up tkinter
+    # root = tk.Tk()
+    # root.withdraw()
+
+    # # Make folder picker dialog appear on top of other windows
+    # root.wm_attributes('-topmost', 1)
+
+    # sidebar
+    with st.sidebar:
+        # show folder picker dialog
+        # st.title('Select Folder')
+        # folderClicked = st.button('Folder Picker')
+
+        currentPath = os.getcwd()
+
+        directory = st.text_input('Enter folder path', currentPath)
+        directory = directory.strip()
+
+        processBtnClicked = st.button('Process files')
+        if processBtnClicked:
+            if not os.path.exists(directory):
+                st.error("Path doesn't exist!")
+            else:
+                # directory = filedialog.askdirectory(master=root)
+                if isinstance(directory, str) and directory:
+                    st.session_state[f"directory"] = directory
+                    st.session_state[f"vectorstoreCount"] += 1
+                    st.session_state[f"qaCount"] += 1
+                    st.session_state[f"user_input"] = []
+                    st.session_state[f"generated"] = []
+
+        # show folder selected
+        if st.session_state[f"directory"]:
+            st.code(st.session_state[f"directory"])
+
+            vectorstore = processDocuments(st.session_state[f"directory"], st.session_state[f"vectorstoreCount"])
+            qa = getQA(vectorstore, st.session_state[f"qaCount"])
+
+            # show clear chat history button
+            if vectorstore:
+                clearMemoryClicked = st.button("🧹 Reset chat history")
+                if clearMemoryClicked:
+                    st.session_state[f"qaCount"] += 1
+                    st.session_state[f"user_input"] = []
+                    st.session_state[f"generated"] = []
+
+                    qa = getQA(vectorstore, st.session_state[f"qaCount"])
+
+    # load previous chat history
+    if st.session_state[f"generated"]:
+        size = len(st.session_state[f"generated"])
+        # Display all exchanges
+        for i in range(0, size):
+            with st.chat_message("user"):
+                st.write(st.session_state[f"user_input"][i])
+            with st.chat_message("assistant"):
+                st.write(st.session_state[f"generated"][i])
+
+    # user chat
+    user_input = st.chat_input("What coding issue can I help you resolve today?")
+    if user_input:
+        with st.chat_message("user"):
+            st.write(user_input)
+            st.session_state[f"user_input"].append(user_input)
+        with st.chat_message("assistant"):
+            with st.spinner("Generating..."):
+                stream_handler = StreamHandler(st.empty())
+                if qa:
+                    print("Using QA")
+                    result = qa(
+                        {"question": user_input},
+                        callbacks=[stream_handler]
+                    )["answer"]
+                else:
+                    print("Using LLM only")
+                    result = llm_chain(
+                        {"question": user_input},
+                        callbacks=[stream_handler]
+                    )
+
+                result = result.replace('"', '')
+                st.session_state[f"generated"].append(result)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
